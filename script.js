3b0da8a656aa5be0d56c734b5ed55cf65f49fff3
Florin Miron <mironflorin123@yahoo.com>
2023-11-08 23:30:36 +0200

    change from inner to create element

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const listInput = document.getElementById('to-do-input');
 const addBtn = document.getElementById('add-btn');
-const removeBtn = document.querySelector('.remove-task-btn');
 const listContainer = document.getElementById('list-container');
-
-
+const removeBtns = document.querySelectorAll('.remove-task-btn');
 
 function addTask(e) {
     e.preventDefault();
@@ -12,15 +10,28 @@ function addTask(e) {
     } else {
         const taskValue = listInput.value;
         const task = document.createElement('li');
-        task.innerHTML = `
-           <i class="fa-solid fa-circle"></i>
-                    <p>${taskValue}</p>
-                    <button class="remove-task-btn"><i class="fa-solid fa-circle-xmark"></i></button>`;
+        task.setAttribute('class', 'list-task');
+        const checkCircle = document.createElement('i');
+        checkCircle.setAttribute('class', 'fa-solid fa-circle');
+        const listContent = document.createElement('p');
+        listContent.textContent = taskValue;
+        const removeBtn = document.createElement('button');
+        removeBtn.setAttribute('class', 'remove-task-btn');
+        const removeBtnIcon = document.createElement('i');
+        removeBtnIcon.setAttribute('class', 'fa-solid fa-circle-xmark');
+        removeBtn.appendChild(removeBtnIcon);
+        task.append(checkCircle, listContent, removeBtn);
         listContainer.appendChild(task);
         listInput.value = '';
     }
-    
 }
 
+
+removeBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const listTask = e.currentTarget.parentElement;
+        listTask.remove();
+    })
+})
+
 addBtn.addEventListener('click', addTask);
-removeBtn.addEventListener('click', removeTask);
